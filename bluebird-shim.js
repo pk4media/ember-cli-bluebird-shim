4f6c579f8ccfb09e7f916fad12711fb685abdeda
David Negstad <dnegstad@gmail.com>
2015-04-23 16:35:59 -0700

    Didn't include generateModule

--- bluebird-shim.js
+++ bluebird-shim.js
@@ -1,3 +1,13 @@
 (function() {
+  /* globals define, Promise */
+
+  function generateModule(name, values) {
+    define(name, [], function() {
+      'use strict';
+
+      return values;
+    });
+  }
+
   generateModule('bluebird', { 'default': Promise.noConflict()});
 })();
